4e475d19bb5341d84b62d90254db819c9cc7a0e5
Shubham011190 <shubham.011190@gmail.com>
2024-03-16 15:14:59 +0530

    Initial commit

---
+++ CloudBees-Train/src/main/java/com/Shubham/CloudBees/CloudBeesTrainApplication.java
@@ -0,0 +1,13 @@
+package com.Shubham.CloudBees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBeesTrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBeesTrainApplication.class, args);
+	}
+
+}
